cf55b9de89b2dc0017c35c4619253309d345aa38
moura1001 <gmouraneto01@gmail.com>
2024-04-17 12:08:28 -0300

    new: consulta 6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente

--- analise.sql
+++ analise.sql
@@ -12,3 +12,6 @@ SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Ano = 1997;
 
 -- 5 - Buscar os filmes lançados APÓS o ano 2000
 SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Ano > 2000;
+
+-- 6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao ASC;
