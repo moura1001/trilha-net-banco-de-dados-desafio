1d26a4c1529e29aef0f58d2fd764d7054513efbb
moura1001 <gmouraneto01@gmail.com>
2024-04-17 12:23:14 -0300

    new: consulta 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome

--- analise.sql
+++ analise.sql
@@ -18,3 +18,6 @@ SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Duracao > 100 AND Duracao < 150 
 
 -- 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
 SELECT Ano, COUNT(*) Quantidade FROM dbo.Filmes GROUP BY Ano ORDER BY Quantidade DESC;
+
+-- 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT PrimeiroNome, UltimoNome FROM dbo.Atores WHERE Genero = 'M';
