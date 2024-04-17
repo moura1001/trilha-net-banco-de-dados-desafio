35355c8b6466b2e511b2ab25e5714fdac01b037b
moura1001 <gmouraneto01@gmail.com>
2024-04-17 12:27:48 -0300

    new: consulta 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome

--- analise.sql
+++ analise.sql
@@ -21,3 +21,6 @@ SELECT Ano, COUNT(*) Quantidade FROM dbo.Filmes GROUP BY Ano ORDER BY Quantidade
 
 -- 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
 SELECT PrimeiroNome, UltimoNome FROM dbo.Atores WHERE Genero = 'M';
+
+-- 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT PrimeiroNome, UltimoNome FROM dbo.Atores WHERE Genero = 'F' ORDER BY PrimeiroNome;
