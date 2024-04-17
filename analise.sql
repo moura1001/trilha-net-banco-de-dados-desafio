60c68045404d71da3bbed0b557ed86a9dea5407b
moura1001 <gmouraneto01@gmail.com>
2024-04-17 12:00:20 -0300

    new: consulta 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração

--- analise.sql
+++ analise.sql
@@ -3,3 +3,6 @@ SELECT Nome, Ano FROM dbo.Filmes;
 
 -- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT Nome, Ano FROM dbo.Filmes ORDER BY Ano ASC;
+
+-- 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Nome = 'De Volta para o Futuro';
