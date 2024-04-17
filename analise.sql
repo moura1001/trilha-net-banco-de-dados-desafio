f178cf0deb1584401a1b1730051a9c5159d10ade
moura1001 <gmouraneto01@gmail.com>
2024-04-17 11:57:23 -0300

    new: consulta 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano

--- analise.sql
+++ analise.sql
@@ -1,2 +1,5 @@
 -- 1 -> Buscar o nome e ano dos filmes
 SELECT Nome, Ano FROM dbo.Filmes;
+
+-- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano FROM dbo.Filmes ORDER BY Ano ASC;
